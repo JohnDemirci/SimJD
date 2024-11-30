44bced17f53919c1eb7547dc55299aac160bdea5
John Demirci <john.demirci@smartthings.com>
2024-11-29 21:32:13 -0800

    Initial Commit

---
+++ SimJD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimJD
+//
+//  Created by John Demirci on 11/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimJD/SimJDApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimJDApp.swift
+//  SimJD
+//
+//  Created by John Demirci on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SimJDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
